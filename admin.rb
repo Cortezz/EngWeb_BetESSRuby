a9503f9a891d64a0031b78c82beed1270beab037
José Cortez <Jospcortez@gmail.com>
2016-03-01 22:32:00 +0000

    Whoops

--- admin.rb
+++ admin.rb
@@ -2,13 +2,9 @@ $LOAD_PATH << '.'
 require "user"
 
 class Admin < User
+
     def initialize (name, email, pwd, coco)
       super(name,email,pwd)
-      @caca = coco
     end
 
-    def printCaca
-      @caca
-    end
-    
 end
