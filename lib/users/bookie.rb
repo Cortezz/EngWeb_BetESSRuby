c808174879b386eddf683e909effba382c8f4e93
José Cortez <Jospcortez@gmail.com>
2016-03-02 13:29:11 +0000

    reorganizing files

---
+++ lib/users/bookie.rb
@@ -0,0 +1,42 @@
+$LOAD_PATH << '.'
+require 'user'
+require 'set'
+
+module Users
+
+  class Bookie < User
+
+    def initialize(name, email, pwd)
+      @name = name
+      @email = email
+      @password = pwd
+      @subscribedEvents = Set.new([])
+    end
+
+    ## Getters
+    def getSubscribedEvents
+      @subscribedEvents
+    end
+
+    ################ EVENTS ##################
+
+    # Adds an event to the bookie's subscribed events list
+    def subscribeTo (event)
+      @subscribedEvents.add(event)
+    end
+
+    # Removes an event, given its ID, from the subscribed events list
+    def unsubscribeTo (event)
+      @subscribedEvents.delete?(event)
+    end
+    #########################################
+
+    def toString
+      s = super + "Subscribed events: "
+      @subscribedEvents.each {|event| s << event.to_s << ";"}
+      s
+    end
+
+
+  end
+end
---
+++ lib/users/user.rb
@@ -0,0 +1,50 @@
+#Class which represents the user
+class User
+    def initialize(name, email, pwd)
+      @name = name
+      @email = email
+      @password = pwd
+      @notifications = []
+    end
+
+    def getEmail
+      @email
+    end
+
+    def getName
+      @name
+    end
+
+    def getPassword
+      @password
+    end
+
+    def toString
+      "Name: #{@name}\nE-mail: #{@email}\nPassword: #{@password}\n"
+    end
+
+    ################# NOTIFICATIONS ####################
+
+    # Pushes a notification to the notification list
+    def pushNotification(notification)
+      @notifications.push(notification)
+    end
+
+    # Returns a string with all the notifications in it
+    def notificationList
+      s = "List of notifications:\n"
+      @notifications.each {|x| s << x << "\n"}
+      s
+    end
+
+    # Returns the amount of notifications
+    def amountOfNotifications
+      @notifications.length
+    end
+
+    # Empties the notification list
+    def removeNotifications
+      @notifications.clear
+    end
+    #######################################################
+end
